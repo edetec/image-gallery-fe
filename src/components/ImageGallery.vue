e5302d49a94e0cdb7b8aec57d1a053b71f45a5a2
Eder Soares <eder@esss.co>
2020-08-22 16:29:47 -0300

    Set up Vue with vuetify

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="6" offset-sm="3">
+      <h1>Images Grid</h1>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
